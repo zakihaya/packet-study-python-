915b67f72e4a5c809de4b5dd2a8cc0b4ffdc84fd
hayazaki <toru.hayazaki@gmail.com>
2022-02-17 13:55:56 +0900

    pingパケットの送信、IPアドレスからMACアドレスを取得するパケット送信

--- scapy_sample.py
+++ scapy_sample.py
@@ -1,14 +1,30 @@
-from socket import IPV6_RECVPATHMTU
-from telnetlib import IP
 from scapy.all import *
 
 # 参考
 # https://qiita.com/Howtoplay/items/4080752d0d8c7a9ef2aa
 
-icmp = ICMP(code=2)
-icmp.show()
+# print("====")
+# arp = ARP()
+# arp.show()
+# print("====")
+# ls(ARP)
+# print("====")
+# ls(Ether)
+# print("====")
+# ls(IP)
 
-arp = ARP()
-arp.show()
+# Googleにpingを送信する
+# / で区切ってパケットを生成する
+# 下のping変数は、IPヘッダとICMPヘッダ
+ping = IP(dst="www.google.com") / ICMP()
+# sr1 - レイヤ3でパケットを送信し，その応答の1つ目がレスポンスとして返ってくる
+ans = sr1(ping)
+ans.show()
 
-ls(IP)
+print("==========")
+
+# 特定のIPアドレスのMACアドレスを得るためのARP要求を行う
+arp = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst="192.168.50.174")
+# srp1 - レイヤ2でパケットを送信する、pが入っているメソッドはレイヤ2でパケットを扱う
+res = srp1(arp)
+res.show()
